77ae83e1681b88de19b0421a51864fe4dcc1f654
Henry Taeschner <henry.taeschner@capgemini.com>
2024-08-03 10:46:45 +0200

    fix: styling select buttons and improve search result layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,25 +1,15 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { TranslateModule, TranslateService } from '@ngx-translate/core'
-import { provideErrorTailorConfig } from '@ngneat/error-tailor'
+import { TranslateModule } from '@ngx-translate/core'
 
-import { AutoCompleteModule } from 'primeng/autocomplete'
-import { ConfirmDialogModule } from 'primeng/confirmdialog'
-import { ConfirmPopupModule } from 'primeng/confirmpopup'
-import { ConfirmationService } from 'primeng/api'
 import { DataViewModule } from 'primeng/dataview'
 import { DialogModule } from 'primeng/dialog'
 import { DropdownModule } from 'primeng/dropdown'
 import { InputTextModule } from 'primeng/inputtext'
-import { InputTextareaModule } from 'primeng/inputtextarea'
-import { KeyFilterModule } from 'primeng/keyfilter'
 import { ListboxModule } from 'primeng/listbox'
-import { MultiSelectModule } from 'primeng/multiselect'
-import { PasswordModule } from 'primeng/password'
 import { SelectButtonModule } from 'primeng/selectbutton'
 import { TableModule } from 'primeng/table'
-import { TabViewModule } from 'primeng/tabview'
 import { ToastModule } from 'primeng/toast'
 
 import { PortalCoreModule } from '@onecx/portal-integration-angular'
@@ -30,76 +20,35 @@ import { LabelResolver } from './label.resolver'
   declarations: [],
   imports: [
     PortalCoreModule.forMicroFrontend(),
-    AutoCompleteModule,
     CommonModule,
-    ConfirmDialogModule,
-    ConfirmPopupModule,
     DataViewModule,
     DialogModule,
     DropdownModule,
     FormsModule,
     InputTextModule,
-    InputTextareaModule,
-    KeyFilterModule,
     ListboxModule,
-    MultiSelectModule,
-    PasswordModule,
     ReactiveFormsModule,
     SelectButtonModule,
     TableModule,
-    TabViewModule,
     ToastModule,
     TranslateModule
   ],
   exports: [
-    AutoCompleteModule,
     CommonModule,
-    ConfirmDialogModule,
-    ConfirmPopupModule,
     DataViewModule,
     DialogModule,
     DropdownModule,
     FormsModule,
     InputTextModule,
-    InputTextareaModule,
-    KeyFilterModule,
     ListboxModule,
-    MultiSelectModule,
-    PasswordModule,
     ReactiveFormsModule,
     SelectButtonModule,
     TableModule,
-    TabViewModule,
     ToastModule,
     TranslateModule
   ],
   //this is not elegant, for some reason the injection token from primeng does not work across federated module
-  providers: [
-    ConfirmationService,
-    LabelResolver,
-    provideErrorTailorConfig({
-      controlErrorsOn: { async: true, blur: true, change: true },
-      errors: {
-        useFactory: (i18n: TranslateService) => {
-          return {
-            required: () => i18n.instant('VALIDATION.ERRORS.EMPTY_REQUIRED_FIELD'),
-            maxlength: ({ requiredLength }) =>
-              i18n.instant('VALIDATION.ERRORS.MAXIMUM_LENGTH').replace('{{chars}}', requiredLength),
-            minlength: ({ requiredLength }) =>
-              i18n.instant('VALIDATION.ERRORS.MINIMUM_LENGTH').replace('{{chars}}', requiredLength),
-            pattern: () => i18n.instant('VALIDATION.ERRORS.PATTERN_ERROR')
-          }
-        },
-        deps: [TranslateService]
-      },
-      //this is required because primeng calendar wraps things in an ugly way
-      blurPredicate: (element: Element) => {
-        return ['INPUT', 'TEXTAREA', 'SELECT', 'CUSTOM-DATE', 'P-CALENDAR', 'P-DROPDOWN'].some(
-          (selector) => element.tagName === selector
-        )
-      }
-    })
-  ],
+  providers: [LabelResolver],
   schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SharedModule {}
